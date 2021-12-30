362b60308f40f02e4e202c2ed6d9b367a0f85b60
Nuno Bispo <nuno.m.bispo@gmail.com>
2021-12-30 21:34:03 +0100

    Update models.py

--- api/models.py
+++ api/models.py
@@ -14,7 +14,7 @@ class TaskModel(BaseModel):
     class Config:
         allow_population_by_field_name = True
         schema_extra = {
-            "task": {
+            "example": {
                 "id": "00010203-0405-0607-0809-0a0b0c0d0e0f",
                 "name": "My task name",
                 "description": "My description",
@@ -32,7 +32,7 @@ class TaskUpdateModel(BaseModel):
     # The config class allows to document our model's schema
     class Config:
         schema_extra = {
-            "task": {
+            "example": {
                 "name": "My task name",
                 "description": "My description",
                 "is_finished": False
